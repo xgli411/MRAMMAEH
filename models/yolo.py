2cb020968c49334964d87f4e45033cce92abf031
xgli411 <148942023+xgli411@users.noreply.github.com>
2024-10-05 17:57:07 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -321,13 +321,13 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in {
                 Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x, C2f,
-                C3_Res2Block}:
+                MRAM}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
 
             args = [c1, c2, *args[1:]]
-            if m in {BottleneckCSP, C3, C3TR, C3Ghost, C3x, C2f, C3_Res2Block}:
+            if m in {BottleneckCSP, C3, C3TR, C3Ghost, C3x, C2f, MRAM}:
                 args.insert(2, n)  # number of repeats
                 n = 1
         elif m is nn.BatchNorm2d:
